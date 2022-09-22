705f95d1496a220b0922e70b8e628f64263947d8
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-09-22 13:44:16 -0400

    Add files via upload

--- Poligonos/src/Pentagono.java
+++ Poligonos/src/Pentagono.java
@@ -1,4 +1,17 @@
 
 public class Pentagono {
+    int ladoA;
+    int ladoB;
+    int ladoC;
+    int ladoD;
+    int ladoE;
 
+
+    public double area(){
+        if (ladoA ==  ladoB && ladoB == ladoC && ladoC == ladoD && ladoD == ladoE){
+
+            perimetro()
+        }
+
+    }
 }
